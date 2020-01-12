2697d31662def90f97f60650732b887acdca5912
elena <742042@gmail.com>
2020-01-12 09:56:10 -0800

    changed method self.contact_creation

--- test_add_contact.py
+++ test_add_contact.py
@@ -13,8 +13,8 @@ class TestAddContact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.contact_creation(wd)
+        self.login(wd, username="admin", password="secret")
+        self.contact_creation(wd, first_name="Elena", last_name="Talley", address="Moscow", email="g345@gmail.com")
         self.return_to_contact_page(wd)
         self.logout(wd)
 
@@ -24,37 +24,37 @@ class TestAddContact(unittest.TestCase):
     def return_to_contact_page (self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def contact_creation (self, wd):
+    def contact_creation (self, wd, first_name, last_name, address, email):
         # init new contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact's form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Elena")
+        wd.find_element_by_name("firstname").send_keys(first_name)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Talley")
+        wd.find_element_by_name("lastname").send_keys(last_name)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Moscow")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("g345@gmail.com")
+        wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_name("theform").click()
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def login (self, wd):
+    def login (self, wd, username, password):
         # login
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page (self, wd):
