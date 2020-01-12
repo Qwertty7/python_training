743ea17311bbe51331be4817ab61e48e52fbae97
elena <742042@gmail.com>
2020-01-12 08:55:16 -0800

    created test add_contact, deleted not imports and class that not using in code, changed var driver to wd

---
+++ test_add_contact.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        # change var name from driver to wd (use refactor-rename)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    # method: test add group
+    def test_add_contact(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/index.php")
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("Elena")
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("Talley")
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("Moscow")
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("g345@gmail.com")
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        wd.find_element_by_link_text("home page").click()
+        wd.find_element_by_link_text("Logout").click()
+    
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+
+    
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
