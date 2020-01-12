dabdb987016fef28c9a0b71bffafad2357e1863c
elena <742042@gmail.com>
2020-01-12 09:28:26 -0800

    grouped test by created methods (used refactor)

--- test_add_contact.py
+++ test_add_contact.py
@@ -7,19 +7,27 @@ class TestAddContact(unittest.TestCase):
     def setUp(self):
         # change var name from driver to wd (use refactor-rename)
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(40)
 
     # method: test add group
     def test_add_contact(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.contact_creation(wd)
+        self.return_to_contact_page(wd)
+        self.logout(wd)
+
+    def logout (self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_contact_page (self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def contact_creation (self, wd):
+        # init new contact creation
         wd.find_element_by_link_text("add new").click()
+        # fill contact's form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Elena")
@@ -37,10 +45,21 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("g345@gmail.com")
         wd.find_element_by_name("theform").click()
+        # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        wd.find_element_by_link_text("home page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def login (self, wd):
+        # login
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page (self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
