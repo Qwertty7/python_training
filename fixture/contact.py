cd8f60ec1f5c4e544042ef70bf93cad09d240326
elena <742042@gmail.com>
2020-01-19 21:36:29 -0800

    created class contact in fixture

---
+++ fixture/contact.py
@@ -0,0 +1,37 @@
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_contact_page()
+
+    def open_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
--- test/test_contact.py
+++ test/test_contact.py
@@ -15,19 +15,19 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact_creation(Contact(first_name="Elena", last_name="Talley", address="Moscow", email="g345@gmail.com"))
+    app.contact.create(Contact(first_name="Elena", last_name="Talley", address="Moscow", email="g345@gmail.com"))
     app.session.logout()
 
 
 def test_add_empty_contact (app):
     app.session.login(username="admin", password="secret")
-    app.contact_creation(Contact(first_name="", last_name="", address="", email=""))
+    app.contact.create(Contact(first_name="", last_name="", address="", email=""))
     app.session.logout()
 
 
 def test_add_contact_with_empty_address_and_email(app):
     app.session.login(username="admin", password="secret")
-    app.contact_creation(Contact(first_name="Petr", last_name="Ivanov", address="", email=""))
+    app.contact.create(Contact(first_name="Petr", last_name="Ivanov", address="", email=""))
     app.session.logout()
 
 
