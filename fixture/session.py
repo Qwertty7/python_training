21d424ee8251deb089b76f9dd1b634edd74e12ba
elena <742042@gmail.com>
2020-01-22 20:50:37 -0800

    added scope sessiion to conftest.py

--- fixture/session.py
+++ fixture/session.py
@@ -5,12 +5,12 @@ class SessionHelper:
     def __init__(self, app):
         self.app = app
 
-    def login(self, username, password):
+    def login(self, user, password):
         wd = self.app.wd
         self.app.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("user").send_keys(user)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
@@ -19,3 +19,4 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,12 +4,12 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
+    app.session.login(user="admin", password="secret")
     app.group.create(Group(name="fhdjsk", header="fdkjlds", footer="fldsjf"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
+    app.session.login(user="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
