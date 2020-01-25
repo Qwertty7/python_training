4589bd3b58952e375550cbc1e2c8ee3e680c5137
elena <742042@gmail.com>
2020-01-24 19:48:18 -0800

    failed

--- model/group.py
+++ model/group.py
@@ -1,8 +1,8 @@
 class Group:
 # создаем конструктор в классе Group
-# записыает значение парамерта в свойсвтва объекта стр №6-8
+# записыает значение парамерта в свойства объекта
 
-    def __init__(self, name, header, footer) :
+    def __init__(self, name=None, header=None, footer=None):
         self.name = name
         self.header = header
         self.footer = footer
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,5 +8,7 @@ def test_add_group(app):
     app.group.create(Group(name="fhdjsk", header="fdkjlds", footer="fldsjf"))
     app.session.logout()
 
-
-
+def test_empty_group(app):
+    app.session.login(user="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
