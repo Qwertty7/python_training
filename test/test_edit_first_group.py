5752efcbba01840bc012cbda4b255822e9ac6348
elena <742042@gmail.com>
2020-01-25 13:18:44 -0800

    modified login/logout

--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -2,5 +2,4 @@
 
 def test_edit_first_group(app):
     app.session.login(user="admin", password="secret")
-    app.group.edit_first_group()
     app.session.logout()
--- test/test_print_all_groups.py
+++ test/test_print_all_groups.py
@@ -1,6 +1,4 @@
 
 
 def test_print_all_groups(app):
-    app.session.login(user="admin", password="secret")
     app.group.print_all_groups()
-    app.session.logout()
